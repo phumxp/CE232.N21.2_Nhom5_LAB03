f1667e5c492ff8d8907c0971a78298c2aa02ca4b
MXP1710 <phumaiboyzzz@gmail.com>
2023-05-23 21:35:03 +0700

    add bai 4

---
+++ BAI4/main.c
@@ -0,0 +1,418 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "driver/i2c.h"
+#include "ssd1306.h"
+#include "font8x8_basic.h"
+
+#define DEVICE_NAME "Lớp 2 - Nhóm 5"
+#define ESP_APP_ID 0x55
+#define PROFILE_NUM 1
+#define PROFILE_APP_IDX 0
+#define SVC_INST_ID 0
+#define STUDENT_ID_LENGTH 8
+
+#define ADV_CONFIG_FLAG (1 << 0)
+#define SCAN_RSP_CONFIG_FLAG (1 << 1)
+
+#define I2C_MASTER_SDA_IO 5
+#define I2C_MASTER_SCL_IO 4
+#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_RX_BUF_DISABLE 0
+#define I2C_MASTER_TX_BUF_DISABLE 0
+
+typedef struct gatts_profile_inst
+{
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+} gatts_profile_inst;
+enum
+{
+    IDX_SVC,
+
+    IDX_CHAR,
+    IDX_CHAR_VAL,
+    IDX_CHAR_CFG,
+
+    HRS_IDX_NB,
+};
+
+static uint8_t adv_config_done = 0;
+static uint8_t numberOfWrites = 0;
+
+static uint8_t service_uuid[16] = {
+    0xAB,
+    0xCD,
+    0xEF,
+    0x12,
+    0x34,
+    0x56,
+    0x78,
+    0x90,
+    0xAB,
+    0xCD,
+    0xEF,
+    0x00,
+    0x00,
+    0x00,
+    0x00,
+    0x00};
+
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x20,
+    .max_interval = 0x40,
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(service_uuid),
+    .p_service_uuid = service_uuid,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT)};
+
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x20,
+    .max_interval = 0x40,
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = 16,
+    .p_service_uuid = service_uuid,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT)};
+
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min = 0x40,
+    .adv_int_max = 0x40,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY};
+
+void ssd1306_init();
+void ssd1306_display_clear();
+void ssd1306_display_text(const void *arg_text, int page);
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+uint16_t gatt_db_handle_table[HRS_IDX_NB];
+
+static gatts_profile_inst profile = {
+    .gatts_cb = gatts_profile_event_handler,
+    .gatts_if = ESP_GATT_IF_NONE};
+
+/* Service */
+static const uint16_t GATTS_SERVICE_UUID = 0xABCD;
+static const uint16_t CHAR_UUID = 0x1234;
+
+static const uint16_t primary_service_uuid = ESP_GATT_UUID_PRI_SERVICE;
+static const uint16_t character_declaration_uuid = ESP_GATT_UUID_CHAR_DECLARE;
+static const uint16_t character_user_description = ESP_GATT_UUID_CHAR_DESCRIPTION;
+static const uint8_t char_prop_read_write = ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_READ;
+static const uint8_t char_name[] = "Please input ID of each team member";
+static const uint8_t studentID_value[STUDENT_ID_LENGTH] = {};
+
+static const esp_gatts_attr_db_t gatt_db[HRS_IDX_NB] =
+    {
+        // Service Declaration
+        [IDX_SVC] =
+            {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&primary_service_uuid, ESP_GATT_PERM_READ, sizeof(uint16_t), sizeof(GATTS_SERVICE_UUID), (uint8_t *)&GATTS_SERVICE_UUID}},
+
+        /* Characteristic Declaration */
+        [IDX_CHAR] =
+            {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ, sizeof(uint8_t), sizeof(uint8_t), (uint8_t *)&char_prop_read_write}},
+
+        /* Characteristic Value */
+        [IDX_CHAR_VAL] =
+            {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&CHAR_UUID, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, 
+            STUDENT_ID_LENGTH, sizeof(studentID_value), (uint8_t *)studentID_value}},
+
+        /* Characteristic User Descriptor */
+        [IDX_CHAR_CFG] =
+            {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_user_description, ESP_GATT_PERM_READ, sizeof(char_name), sizeof(char_name), (uint8_t *)char_name}}};
+
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+void app_main(void)
+{
+    int i2c_master_port = 0;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    ssd1306_init();
+    ssd1306_display_clear();
+
+    // Khởi tạo phân vùng NVS để sử dụng cho bluetooth driver
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+    // Xóa bỏ vùng nhớ dùng để quản lý bluetooth cổ điển
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    // Thiết lập chế độ cho BLE
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+    // Khởi tạo bluedroid và cho phép bluedroid hoạt động để sử dụng BLE
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
+
+    ESP_ERROR_CHECK(esp_ble_gatts_register_callback(gatts_event_handler));
+
+    ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
+
+    ESP_ERROR_CHECK(esp_ble_gatts_app_register(ESP_APP_ID));
+
+    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;
+    esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(esp_ble_auth_req_t));
+}
+
+void ssd1306_init()
+{
+    esp_err_t espRc;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+    i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+    i2c_master_write_byte(cmd, 0x14, true);
+    i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true);
+    i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_NORMAL, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_OFF, true);
+    i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+    i2c_master_stop(cmd);
+    espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+    if (espRc == ESP_OK)
+    {
+        printf("OLED configured successfully\n");
+    }
+    else
+    {
+        printf("OLED configuration failed. code: 0x%.2X\n", espRc);
+    }
+    i2c_cmd_link_delete(cmd);
+}
+void ssd1306_display_clear()
+{
+    i2c_cmd_handle_t cmd;
+    uint8_t clear[128];
+    for (uint8_t i = 0; i < 128; i++)
+    {
+        clear[i] = 0;
+    }
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+        i2c_master_write_byte(cmd, 0xB0 | i, true);
+
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+        i2c_master_write(cmd, clear, 128, true);
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+    }
+}
+void ssd1306_display_text(const void *arg_text, int page)
+{
+    char *text = (char *)arg_text;
+    uint8_t text_len = strlen(text);
+
+    i2c_cmd_handle_t cmd;
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+    i2c_master_write_byte(cmd, 0x00, true);
+    i2c_master_write_byte(cmd, 0x10, true);
+    i2c_master_write_byte(cmd, 0xB0 | page, true);
+
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    for (uint8_t i = 0; i < text_len; i++)
+    {
+        if (text[i] == '\n')
+        {
+            cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+            i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+            i2c_master_write_byte(cmd, 0x00, true);
+            i2c_master_write_byte(cmd, 0x10, true);
+            i2c_master_write_byte(cmd, 0xB0 | ++page, true);
+
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+            i2c_cmd_link_delete(cmd);
+        }
+        else
+        {
+            cmd = i2c_cmd_link_create();
+            i2c_master_start(cmd);
+            i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+            i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+            i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
+
+            i2c_master_stop(cmd);
+            i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+            i2c_cmd_link_delete(cmd);
+        }
+    }
+}
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    switch (event)
+    {
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+        adv_config_done &= (~ADV_CONFIG_FLAG);
+        if (adv_config_done == 0)
+        {
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT:
+        adv_config_done &= (~SCAN_RSP_CONFIG_FLAG);
+        if (adv_config_done == 0)
+        {
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS)
+        {
+            printf("Advertising start failed\n");
+        }
+        else
+        {
+            printf("Advertising start successfully\n\n");
+        }
+        break;
+    case ESP_GAP_BLE_AUTH_CMPL_EVT:
+        if (param->ble_security.auth_cmpl.success)
+        {
+            printf("Pair status = success\n\n");
+        }
+        else
+        {
+            printf("Pair status = fail, reason = 0x%x\n", param->ble_security.auth_cmpl.fail_reason);
+        }
+        break;
+    default:
+        break;
+    }
+}
+static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
+{
+    switch (event)
+    {
+    case ESP_GATTS_REG_EVT:
+        ESP_ERROR_CHECK(esp_ble_gap_set_device_name(DEVICE_NAME));
+        ESP_ERROR_CHECK(esp_ble_gap_config_adv_data(&adv_data));
+        adv_config_done |= ADV_CONFIG_FLAG;
+        ESP_ERROR_CHECK(esp_ble_gap_config_adv_data(&scan_rsp_data));
+        adv_config_done |= SCAN_RSP_CONFIG_FLAG;
+        ESP_ERROR_CHECK(esp_ble_gatts_create_attr_tab(gatt_db, gatts_if, HRS_IDX_NB, SVC_INST_ID));
+        break;
+    case ESP_GATTS_CREAT_ATTR_TAB_EVT:
+        if (param->add_attr_tab.status != ESP_GATT_OK)
+        {
+            printf("Create attribute table failed, error code=0x%x\n", param->add_attr_tab.status);
+        }
+        else if (param->add_attr_tab.num_handle != HRS_IDX_NB)
+        {
+            printf("Create attribute table abnormally, num_handle (%d) \n\
+                        Doesn't equal to HRS_IDX_NB(%d)\n",
+                   param->add_attr_tab.num_handle, HRS_IDX_NB);
+        }
+        else
+        {
+            printf("Create attribute table successfully, the number handle = %d\n", param->add_attr_tab.num_handle);
+            memcpy(gatt_db_handle_table, param->add_attr_tab.handles, sizeof(gatt_db_handle_table));
+            ESP_ERROR_CHECK(esp_ble_gatts_start_service(gatt_db_handle_table[IDX_SVC]));
+        }
+        break;
+    case ESP_GATTS_START_EVT:
+        printf("SERVICE_START_EVT, status %d, service_handle %d\n", param->start.status, param->start.service_handle);
+        break;
+    case ESP_GATTS_CONNECT_EVT:
+        printf("ESP_GATTS_CONNECT_EVT, conn_id = %d\n", param->connect.conn_id);
+        break;
+    case ESP_GATTS_DISCONNECT_EVT:
+        printf("ESP_GATTS_DISCONNECT_EVT, reason = %d\n", param->disconnect.reason);
+        ESP_ERROR_CHECK(esp_ble_gap_start_advertising(&adv_params));
+    case ESP_GATTS_READ_EVT:
+        printf("ESP_GATTS_READ_EVT, handle=0x%d, offset=%d\n\n", param->read.handle, param->read.offset);
+        break;
+    case ESP_GATTS_WRITE_EVT:
+        printf("ESP_GATTS_WRITE_EVT, handle=0x%04x\n", param->write.handle);
+        if (gatt_db_handle_table[IDX_CHAR_VAL] == param->write.handle && param->write.len == STUDENT_ID_LENGTH)
+        {
+            if (numberOfWrites % 8 == 0) ssd1306_display_clear();
+            uint8_t write_data[STUDENT_ID_LENGTH + 1];
+            memcpy(write_data, param->write.value, param->write.len);
+            write_data[STUDENT_ID_LENGTH] = '\0';
+            ssd1306_display_text(write_data, numberOfWrites % 8);
+            numberOfWrites++;
+        }
+        break;
+    default:
+        break;
+    }
+}
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
+{
+    if (event == ESP_GATTS_REG_EVT)
+    {
+        if (param->reg.status == ESP_GATT_OK)
+        {
+            profile.gatts_if = gatts_if;
+        }
+        else
+        {
+            printf("Reg app failed, app_id %04x, status %d\n", param->reg.app_id, param->reg.status);
+            return;
+        }
+    }
+
+    if (gatts_if == ESP_GATT_IF_NONE || gatts_if == profile.gatts_if)
+    {
+        if (profile.gatts_cb)
+        {
+            profile.gatts_cb(event, gatts_if, param);
+        }
+    }
+}
